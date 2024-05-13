9dcf7c92116ac673857afb852b9c3a18e11b9caf
firuzzokirov <firuzzokirov33@gmail.com>
2024-05-13 22:10:45 +0500

    cls

---
+++ app/src/main/java/com/firuz/currencyconvertor/ui/converter/ConverterFragment.kt
@@ -0,0 +1,34 @@
+package com.firuz.currencyconvertor.ui.converter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.firuz.currencyconvertor.databinding.ConverterFragmentBinding
+
+class ConverterFragment : Fragment() {
+
+    private var _binding: ConverterFragmentBinding? = null
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = ConverterFragmentBinding.inflate(layoutInflater,container,false)
+        return _binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
